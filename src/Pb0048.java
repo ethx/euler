3197ff4255c7ec0553f40e738fa261f269589ef8
Thomas Boucher <ethx.fr@gmail.com>
2018-09-26 20:30:35 +0200

    Problem 48

---
+++ src/Pb0048.java
@@ -0,0 +1,20 @@
+/**
+ * https://projecteuler.net/problem=48
+ */
+public class Pb0048 {
+
+    public static void main(String[] args) {
+        // only need the last 10 digits of the solution
+        final long RES_MODULO = 10_000_000_000L;
+
+        long res = 0;
+        for (int i = 1; i < 1_000; ++i) {
+            long tmp = 1;
+            for (int j = 0; j < i; ++j) {
+                tmp = (tmp * i) % RES_MODULO;
+            }
+            res = (res + tmp) % RES_MODULO;
+        }
+        System.out.println(String.format("Pb0066: %d", res));
+    }
+}
